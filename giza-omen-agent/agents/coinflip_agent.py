92be89f56a50235a261075abe32ce03d40ff4247
Gabriel Fior <fior.gbm@gmail.com>
2024-05-14 11:01:18 -0300

    Added more files for running an agent

---
+++ giza-omen-agent/agents/coinflip_agent.py
@@ -0,0 +1,11 @@
+import random
+import typing as t
+
+from prediction_market_agent_tooling.deploy.agent import (
+    Answer,
+    DeployableTraderAgent,
+    Probability,
+)
+from prediction_market_agent_tooling.markets.agent_market import AgentMarket
+
+# from https://github.com/gnosis/prediction-market-agent/blob/main/prediction_market_agent/agents/coinflip_agent/deploy.py
---
+++ scripts/run_agent.py
@@ -0,0 +1,32 @@
+import random
+
+import typer
+from prediction_market_agent_tooling.deploy.agent import Answer, DeployableTraderAgent
+from prediction_market_agent_tooling.gtypes import Probability
+from prediction_market_agent_tooling.markets.agent_market import AgentMarket
+from prediction_market_agent_tooling.markets.markets import MarketType
+
+
+class DeployableCoinFlipAgent(DeployableTraderAgent):
+    def pick_markets(self, markets: t.Sequence[AgentMarket]) -> t.Sequence[AgentMarket]:
+        return random.sample(markets, 1)
+
+    def answer_binary_market(self, market: AgentMarket) -> Answer | None:
+        decision = random.choice([True, False])
+        return Answer(
+            decision=decision,
+            confidence=0.5,
+            p_yes=Probability(float(decision)),
+            reasoning="I flipped a coin to decide.",
+        )
+
+
+def main():
+    print ('start')
+    agent = DeployableCoinFlipAgent()
+    market_type = MarketType.OMEN
+    agent.run(market_type)
+    print ('end')
+
+if __name__ == "__main__":
+    typer.run(main)
